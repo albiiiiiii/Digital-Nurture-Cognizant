ed9a75553bcaf6d46676106c5a6517e70bc3dff1
Albin <90824086+albiiiiiii@users.noreply.github.com>
2024-08-23 17:55:47 +0530

    Delete Exercise 4 directory

--- Exercise 4/text_processing_tool/convert_to_uppercase.py
+++
@@ -1,2 +0,0 @@
-def convert_to_uppercase(text):
-    return text.upper()
--- Exercise 4/text_processing_tool/find_unique_words.py
+++
@@ -1,4 +0,0 @@
-def find_unique_words(text):
-    words = text.split()
-    unique_words = set(words)
-    return unique_words
